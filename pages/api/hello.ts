3f35f8fea28307b0e5b262b5810c17757bc528e6
villapol <villapol@gmail.com>
2023-01-08 16:32:29 -0400

    Se agrega

---
+++ pages/api/hello.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from 'next/server'
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function (req: NextRequest) {
+  return new Response(
+    JSON.stringify({ name: 'John Doe' }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+}
